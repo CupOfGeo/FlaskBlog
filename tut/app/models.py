98273a6002a62f8ecb2a18b2adbd50aefa5950b5
George <gbm@bucknell.edu>
2020-12-29 17:23:47 -0500

    finished chapter 6

--- tut/app/models.py
+++ tut/app/models.py
@@ -2,6 +2,9 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
+
+#DATABASE STUFF
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +12,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow())
 
     def _repr__(self):
         return '<User {}>'.format(self.username)
@@ -18,6 +23,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        #turns email to random picture
+        return 'https:www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- tut/microblog.py
+++ tut/microblog.py
@@ -1,6 +1,6 @@
 from app import app, db
 from app.models import User, Post
 
-@app.shell_context_processors
+#@app.shell_context_processors #giving me errors all of a sudden :(
 def make_shell_context():
         return{'db': db, 'User': User, 'Post': Post}
