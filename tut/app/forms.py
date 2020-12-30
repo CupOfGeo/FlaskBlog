870f938e54be68ab34af2d72c213f3c39f9e02fa
George <gbm@bucknell.edu>
2020-12-29 19:25:23 -0500

    finished chapter 7 created error logs no email logs

--- tut/app/forms.py
+++ tut/app/forms.py
@@ -32,6 +32,16 @@ class RegistrationForm(FlaskForm):
 
 
 class EditProfileForm(FlaskForm):
-    username = StringField('userame', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0,max=140)])
     submit = SubmitField("Submit")
+
+    def __init__(self, orginal_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args,**kwargs)
+        self.orginal_username = orginal_username
+
+    def validate_username(self, username):
+        if username.data != self.orginal_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username')
--- tut/config.py
+++ tut/config.py
@@ -6,3 +6,17 @@ class Config(object):
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATION = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['mazzeogeorge@gmail.com']
+'''
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME=mazzeogeorge@gmail.com
+export MAIL_PASSWORD=...
+'''
